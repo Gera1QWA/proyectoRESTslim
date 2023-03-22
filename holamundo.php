8df2f9e6890ba0a35e73bc57d1e76caa76e822e8
Gera1QWA <91513443+Gera1QWA@users.noreply.github.com>
2023-03-22 01:18:23 -0600

    updaRSW

--- holamundo.php
+++ holamundo.php
@@ -91,6 +91,69 @@ $app->get('/productos/{categoria}', function (Request $request, Response $respon
     
   
 });
+$app->get('/detalles', function(Request $request, Response $response){
+    global $res;
+    global $resp;
+
+    $resp['code'] = 999;
+    $resp['message'] =  $res->obtainMessage('999');
+    $json = json_encode($resp);
+    $response->getBody()->write($json);
+    return $response;
+});
+$app->get('/detalles/{ISBN}', function (Request $request, Response $response, $args) {
+    global $res;
+    global $resp;
+    $isbn = $args['ISBN'];
+    $headers = $request->getHeaderLine('user');  
+    $headers2 = $request->getHeaderLine('pass');
+     //$response->getBody()->write("Hello, $headers");
+    // $response->getBody()->write("Hello, $categoria");
+    
+    if($request->hasHeader('user')){
+        if($res->isUser($headers)){
+            if($res->obtainPass($headers) === md5($headers2)){
+                if($res->isIsbnDd($isbn)){
+                    $resp['code'] = 201;
+                    $resp['message'] = $res->obtainMessage('201');
+                    $resp['status'] = 'success';
+                    $resp['data'] = $res-> obtainDetails($isbn);
+                    $json = json_encode($resp);
+                    $response->getBody()->write($json);
+
+                }else{
+                    $resp['code'] = 304;
+                    $resp['message'] = $res->obtainMessage('304');
+                    $json = json_encode($resp,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                    $response->getBody()->write($json);
+                }
+                
+            
+            }else{
+                $resp['code'] = 501;
+                $resp['message'] =   $res->obtainMessage('501');
+                $json = json_encode($resp);
+                $response->getBody()->write($json);
+            }
+
+        }else{
+            $resp['code'] = 500;
+            $resp['message'] =   $res->obtainMessage('500');
+            $json = json_encode($resp);
+            $response->getBody()->write($json);
+        }
+
+    }else{
+        $resp['code'] = 500;
+        $resp['message'] =  $res->obtainMessage('500');
+        $json = json_encode($resp);
+        $response->getBody()->write($json);
+    }
+
+    return $response;
+    
+  
+});
 
 
 $app->run();
